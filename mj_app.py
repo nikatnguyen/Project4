ca8d0a782419dfb7f657f7e5ca85f29064f43006
Mjkey2 <Mjmarentes2005@gmail.com>
2024-03-10 10:02:03 -0700

    Update mj_app.py

--- mj_app.py
+++ mj_app.py
@@ -2,3 +2,6 @@ import numpy as numpy
 import pandas as pandasimport streamlit as st
 from sklearn import preprocessing
 import pickle
+
+model = pickle.load(open('mj_obesity_nn.pkl', 'rb'))
+encoder_dict = pickle.load(open('encoder.pkl', 'rb'))
